946dbea4c84ccec743ef87f55aa3349ab5b52ac1
hamid4965 <ahangar.hamid@gmail.com>
2017-11-26 18:38:30 -0500

    Add files via upload

--- PreProcessing/plot_wrf_obs.R
+++ PreProcessing/plot_wrf_obs.R
@@ -3,37 +3,65 @@
 rm(list = ls())
 setwd("~/wrf/Holl")
 library('openxlsx')
-############### ploting the wrf downloaded data vs stations
+source("extract_wrf.R")
+
+#-----------------------Ploting temprature--------------------------
+
+JAN <- extract_wrf(2011,01,"tmp","HOL","FALSE")
+FEB <- extract_wrf(2011,02,"tmp","HOL","FALSE")
+MAR <- extract_wrf(2011,03,"tmp","HOL","FALSE")
+APR <- extract_wrf(2011,04,"tmp","HOL","FALSE")
+MAY <- extract_wrf(2011,05,"tmp","HOL","FALSE")
+JUN <- extract_wrf(2011,06,"tmp","HOL","FALSE")
+JUL <- extract_wrf(2011,07,"tmp","HOL","FALSE")
+AUG <- extract_wrf(2011,08,"tmp","HOL","FALSE")
+SEP <- extract_wrf(2011,09,"tmp","HOL","FALSE")
+OCT <- extract_wrf(2011,10,"tmp","HOL","FALSE")
+NOV <- extract_wrf(2011,11,"tmp","HOL","FALSE")
+DEC <- extract_wrf(2011,12,"tmp","HOL","FALSE")
+
+y_2011<-c(JAN,FEB,MAR,APR,MAY,JUN,JUL,AUG,SEP,OCT,NOV,DEC)
+
 
 n = 1 # This is the sheet in the file
 
 Station.Data <- read.xlsx("Holl_2011.xlsx", sheet = n,colNames = T)
 # Read the temprature data [c] at 3m and 30 mins measurments
 tmp <- as.vector(Station.Data$`IRT.2.(N)`)
-
-
-
 tmp_k <- tmp+273.15 
 
 # now we take the 3 hour average of 30 mins data
 tmp_k_reshape <- matrix(tmp_k, nrow = 6)
 obs <- colMeans(tmp_k_reshape)  
-t_obs <- seq(1,length(obs),1)
-plot(t_obs,obs)
-
-var <- "T2_2010_2013.xlsx"
-wrf.data <- as.matrix(read.xlsx(var))
-wrf.2011 <- wrf.data[,366:(366+365-1)]
-wrf<-as.vector(wrf.2011)
-t_wrf <- seq(1,length(wrf),1)
-
+t <- seq(1,length(obs),1)
 plot(t_obs,obs,ty="l")
-lines(t_wrf,wrf,col="red")
+lines(t,y_2011,col="red")
 
-## ploting the precipitation-------------------------------------------------------------
-rm(list = ls())
-setwd("~/wrf/Holl")
-library('openxlsx')
+title(main="WRF vs observed 2011")
+
+
+#--------------------Ploting rainfall [kg/m2/s]---------------------------
+
+JAN <- extract_wrf(2011,01,"prate","HOL","FALSE")
+FEB <- extract_wrf(2011,02,"prate","HOL","FALSE")
+MAR <- extract_wrf(2011,03,"prate","HOL","FALSE")
+APR <- extract_wrf(2011,04,"prate","HOL","FALSE")
+MAY <- extract_wrf(2011,05,"prate","HOL","FALSE")
+JUN <- extract_wrf(2011,06,"prate","HOL","FALSE")
+JUL <- extract_wrf(2011,07,"prate","HOL","FALSE")
+AUG <- extract_wrf(2011,08,"prate","HOL","FALSE")
+SEP <- extract_wrf(2011,09,"prate","HOL","FALSE")
+OCT <- extract_wrf(2011,10,"prate","HOL","FALSE")
+NOV <- extract_wrf(2011,11,"prate","HOL","FALSE")
+DEC <- extract_wrf(2011,12,"prate","HOL","FALSE")
+y_2011<-c(JAN,FEB,MAR,APR,MAY,JUN,JUL,AUG,SEP,OCT,NOV,DEC)
+prate_mont_vec_diff<-diff(y_2011)
+prate_mont_vec_diff <- replace(prate_mont_vec_diff, prate_mont_vec_diff<0, 0)
+prate_mont_vec_diff_kg<-(1/10800)*prate_mont_vec_diff
+n_prate<-as.numeric(length(prate_mont_vec_diff_kg))
+t_prate<-1:n_prate
+
+# importing the observed data 
 n = 1 # This is the sheet in the file
 Station.Data <- read.xlsx("Holl_2011.xlsx", sheet = n,colNames = T)
 # Read the temprature data [c] at 3m and 30 mins measurments
@@ -44,26 +72,63 @@ obs <- colSums(tmp_reshape)
 t_obs <- seq(1,length(obs),1)
 obs_kg <- (1/10800)*obs
 
-var <- "RAINNC_2010_2013.xlsx"
-wrf.data <- as.matrix(read.xlsx(var))
-wrf.2011 <- wrf.data[,366:(366+365-1)]
-wrf<-as.vector(wrf.2011)
-wrf<-diff(wrf)   # convert it from cumulative to mm
-wrf <- replace(wrf, wrf<0, 0)
-wrf_kg<-(1/10800)*wrf
-t_wrf= seq(1,length(wrf),1)
-plot(t_obs,obs,ty="l",ylim = c(0,15),col='blue')
-lines(t_wrf,wrf,col='red')
-legend('topright', legend=c("Observed", "wrf Simulated"),
-       col=c("blue", "red"), lty=1:2, cex=1.2)
+max_obs<-max(obs_kg)
+max_wrf<-max(prate_mont_vec_diff_kg)
 
-plot.new()
-plot(t_obs,obs_kg,ylim = c(0,max(wrf_kg)),col='blue',ty='l', xlab="time (3h)", ylab="Rainfall [ kg/m2/s ]" )
+par(mar=c(5.1,4.5,4.1,2.1))
+plot(t_obs,obs_kg,col='red',ty="l",ylim = c(0,max(c(max_obs,max_wrf))),
+     xlab="time (3h)", ylab="Rainfall [ kg/m2/s ]",cex.lab=1.5)
 title(main="WRF vs observed 2011")
-lines(t_wrf,wrf_kg,col='red')
-
+lines(t_prate,prate_mont_vec_diff_kg,col='blue')
 legend('topright', legend=c("Observed", "WRF Simulated"),
-       col=c("blue", "red"), lty=1:2, cex=1.2)
+       col=c("red", "blue"), lty=1:2, cex=1.2)
+
+#-----------------------Ploting presure--------------------------
+
+
+JAN <- extract_wrf(2011,01,"pres","HOL","FALSE")
+FEB <- extract_wrf(2011,02,"pres","HOL","FALSE")
+MAR <- extract_wrf(2011,03,"pres","HOL","FALSE")
+APR <- extract_wrf(2011,04,"pres","HOL","FALSE")
+MAY <- extract_wrf(2011,05,"pres","HOL","FALSE")
+JUN <- extract_wrf(2011,06,"pres","HOL","FALSE")
+JUL <- extract_wrf(2011,07,"pres","HOL","FALSE")
+AUG <- extract_wrf(2011,08,"pres","HOL","FALSE")
+SEP <- extract_wrf(2011,09,"pres","HOL","FALSE")
+OCT <- extract_wrf(2011,10,"pres","HOL","FALSE")
+NOV <- extract_wrf(2011,11,"pres","HOL","FALSE")
+DEC <- extract_wrf(2011,12,"pres","HOL","FALSE")
+y_2011<-c(JAN,FEB,MAR,APR,MAY,JUN,JUL,AUG,SEP,OCT,NOV,DEC)
+t_pres<-seq(1,length(y_2011),1)
+
+# importing the observed data 
+n = 1 # This is the sheet in the file
+Station.Data <- read.xlsx("Holl_2011.xlsx", sheet = n,colNames = T)
+# Read the temprature data [c] at 3m and 30 mins measurments
+tmp <- Station.Data$PTB110_kP_Avg
+# now we take the 3 hour SUM of 30 mins data
+tmp_reshape <- matrix(tmp, nrow = 6)
+obs <- colMeans(tmp_reshape)
+obs <-obs*1000
+pos <- which( obs<80000)
+obs[pos[1]]<-(obs[pos[1]-1]+obs[pos[1]+1])/2
+obs[pos[2]]<-(obs[pos[2]-1]+obs[pos[2]+1])/2
+
+t_obs <- seq(1,length(obs),1)
+
+
+max_obs<-max(obs)
+max_wrf<-max(y_2011)
+min_obs<-min(obs)
+min_wrf<-min(y_2011)
+par(mar=c(5.1,4.5,4.1,2.1))
+plot(t_obs,obs,col='red',ty="l",ylim = c(min(c(min_obs,min_wrf)),max(c(max_obs,max_wrf))),
+     xlab="time (3h)", ylab="Rressure [ Pa ]",cex.lab=1.5)
+
+title(main="WRF vs observed 2011")
+lines(t_pres,y_2011,col='blue')
+legend('topright', legend=c("Observed", "WRF Simulated"),
+       col=c("red", "blue"), lty=1:2, cex=1.2)
 
 
 
---
+++ PreProcessing/wrf_to_hdf .R
@@ -0,0 +1,175 @@
+wrf_to_hdf <- function(y1,y2,leap){
+  
+  library(openxlsx)
+  library(rhdf5)
+  library(ncdf4)
+  
+  #y1=2010;y2=2015;leap=2012
+  
+  filename_tmp <- paste0("T2_",y1,"_",y2,".xlsx")
+  filename_pres <- paste0("PSFC_",y1,"_",y2,".xlsx")
+  filename_prate <- paste0("RAINNC_",y1,"_",y2,".xlsx")
+  filename_hgt <- paste0("HGT_",y1,"_",y2,".xlsx")
+  filename_ugrd <- paste0("U10_",y1,"_",y2,".xlsx")
+  filename_vgrd <- paste0("V10_",y1,"_",y2,".xlsx")
+  filename_sh <- paste0("Q2_",y1,"_",y2,".xlsx")
+  filename_dlwrf <- paste0("GLW_",y1,"_",y2,".xlsx")
+  filename_nbdsf <- paste0("SWDOWN_",y1,"_",y2,".xlsx")
+  
+  tmp <- as.matrix(read.xlsx(filename_tmp, sheet = 1,colNames = T))
+  pres <-as.matrix(read.xlsx(filename_pres, sheet = 1,colNames = T))
+  prate <- as.matrix(read.xlsx(filename_prate, sheet = 1,colNames = T))
+  hgt <- as.matrix(read.xlsx(filename_hgt, sheet = 1,colNames = T))
+  ugrd <- as.matrix(read.xlsx(filename_ugrd, sheet = 1,colNames = T))
+  vgrd <- as.matrix(read.xlsx(filename_vgrd, sheet = 1,colNames = T))
+  sh <- as.matrix(read.xlsx(filename_sh, sheet = 1,colNames = T))  
+  dlwrf <- as.matrix(read.xlsx(filename_dlwrf, sheet = 1,colNames = T))
+  nbds <- as.matrix(read.xlsx(filename_nbdsf, sheet = 1,colNames = T))
+  
+  
+  for (i in y1:y2){
+    
+    if (i==leap){
+      n_day = 366;leap=(leap+4)
+    } else {n_day=365}
+    
+    if (ncol(tmp)<n_day){
+      tmp_year <- tmp[,1:ncol(tmp)]
+      prate_year <- prate[,1:ncol(prate)]
+      pres_year <- pres[,1:ncol(pres)]
+      hgt_year <- hgt[,1:ncol(hgt)]
+      ugrd_year <- ugrd[,1:ncol(ugrd)]
+      vgrd_year <- vgrd[,1:ncol(vgrd)]
+      sh_year <- sh[,1:ncol(sh)]
+      dlwrf_year <- dlwrf[,1:ncol(dlwrf)]
+      nbds_year <- nbds[,1:ncol(nbds)]
+      
+    } else {
+      tmp_year <- tmp[,1:n_day] 
+      prate_year <- prate[,1:n_day] 
+      pres_year <- pres[,1:n_day]
+      hgt_year <- hgt[,1:n_day]
+      ugrd_year <- ugrd[,1:n_day]
+      vgrd_year <- vgrd[,1:n_day]
+      sh_year <- sh[,1:n_day]
+      dlwrf_year <-  dlwrf[,1:n_day]
+      nbds_year <- nbds[,1:n_day]
+      } 
+    
+ 
+    if (i==y2){ 
+      tmp <- tmp
+      prate <- prate
+      pres <- pres
+      hgt <- hgt
+      ugrd <- ugrd
+      vgrd <- vgrd
+      sh <- sh
+      dlwrf <- dlwrf
+      nbds <-nbds
+    } else {
+      tmp <- tmp[,(n_day+1):ncol(tmp)]
+      prate <- prate[,(n_day+1):ncol(prate)]
+      pres <- pres[,(n_day+1):ncol(pres)]
+      hgt <- hgt[,(n_day+1):ncol(hgt)]
+      ugrd <- ugrd[,(n_day+1):ncol(ugrd)]
+      vgrd <- vgrd[,(n_day+1):ncol(vgrd)]
+      sh <- sh[,(n_day+1):ncol(sh)]
+      dlwrf <- dlwrf[,(n_day+1):ncol(dlwrf)]
+      nbds <- nbds[,(n_day+1):ncol(nbds)]
+    }
+    
+   for (j in 1:12){
+      
+      if (j == 1) {k2 = 31;M="JAN"}
+      if (j == 2) {
+        if (i==leap){k2=29;M="FEB"
+        } else {k2=28;M="FEB"}}
+      if (j == 3) {k2 = 31;M="MAR" }
+      if (j == 4) {k2 = 30;M="APR"}
+      if (j == 5) {k2 = 31;M="MAY"}
+      if (j == 6) {k2 = 30;M="JUN"}
+      if (j == 7) {k2 = 31;M="JUL"}
+      if (j == 8) {k2 = 31;M="AUG"}
+      if (j == 9) {k2 = 30;M="SEP"}
+      if (j == 10) {k2 = 31;M="OCT"}
+      if (j == 11) {k2 = 30;M="NOV"}
+      if (j == 12) {k2 = 31;M="DEC"}
+      
+      n = k2*8
+      if (ncol(tmp_year)<k2){
+        print(paste("Nomber of days fo the",M, "of",i, "is not complete"))
+        print("Process is terminated!!")
+        break
+        
+      }
+      tmp_month <- tmp_year[,1:k2]
+      prate_month <- prate_year[,1:k2]
+      pres_month <- pres_year[,1:k2]
+      hgt_month <- hgt_year[,1:k2]
+      ugrd_month <- ugrd_year[,1:k2]
+      vgrd_month <- vgrd_year[,1:k2]
+      sh_month <- sh_year[,1:k2]
+      dlwrf_month <- dlwrf_year[,1:k2]
+      nbds_month <- nbds_year[,1:k2] 
+      if(j==12){
+      tmp_year=tmp_year
+      prate_year=prate_year
+      pres_year=pres_year
+      hgt_year=hgt_year
+      ugrd_year=ugrd_year
+      vgrd_year=vgrd_year
+      sh_year=sh_year
+      dlwrf_year=dlwrf_year
+      nbds_year=nbds_year
+      } else {
+      tmp_year = tmp_year[,(k2+1):ncol(tmp_year)]
+      prate_year = prate_year[,(k2+1):ncol(prate_year)]
+      pres_year = pres_year[,(k2+1):ncol(pres_year)]
+      hgt_year = hgt_year[,(k2+1):ncol(hgt_year)]
+      ugrd_year = ugrd_year[,(k2+1):ncol(ugrd_year)]
+      vgrd_year = vgrd_year[,(k2+1):ncol(vgrd_year)]
+      sh_year = sh_year[,(k2+1):ncol(sh_year)]
+      dlwrf_year = dlwrf_year[,(k2+1):ncol(dlwrf_year)]
+      nbds_year = nbds_year[,(k2+1):ncol(nbds_year)]
+      }
+      tmp_mont_vec <- as.vector(tmp_month)
+      prate_mont_vec <- as.vector(prate_month)
+      pres_mont_vec <- as.vector(pres_month)
+      hgt_mont_vec <- as.vector(hgt_month)
+      ugrd_mont_vec <- as.vector(ugrd_month)
+      vgrd_mont_vec <- as.vector(vgrd_month)
+      sh_mont_vec <- as.vector(sh_month)
+      dlwrf_mont_vec <- as.vector(dlwrf_month)
+      nbds_mont_vec <- as.vector(nbds_month)
+      
+      filename <- paste0("HOL_",i,"_",M,".h5")
+      # Create new hdf5 file
+      h5createFile(filename)
+      h5createDataset(filename, "tmp", c(n,1,1))
+      h5createDataset(filename, "prate", c(n,1,1))
+      h5createDataset(filename, "pres", c(n,1,1))
+      h5createDataset(filename, "hgt", c(n,1,1))
+      h5createDataset(filename, "ugrd", c(n,1,1))
+      h5createDataset(filename, "vgrd", c(n,1,1))
+      h5createDataset(filename, "sh", c(n,1,1))
+      h5createDataset(filename, "dlwrf", c(n,1,1))
+      h5createDataset(filename, "nbds", c(n,1,1))
+      h5write(tmp_mont_vec, file = filename, name = "/tmp")
+      h5write(prate_mont_vec, file = filename, name = "/prate")
+      h5write(pres_mont_vec, file = filename, name = "/pres")
+      h5write(hgt_mont_vec, file = filename, name = "/hgt")
+      h5write(ugrd_mont_vec, file = filename, name = "/ugrd")
+      h5write(vgrd_mont_vec, file = filename, name = "/vgrd")
+      h5write(sh_mont_vec, file = filename, name = "/sh")
+      h5write(dlwrf_mont_vec, file = filename, name = "/dlwrf")
+      h5write(nbds_mont_vec, file = filename, name = "/nbds")
+      
+      
+      
+    }
+    
+  }
+  H5close()
+  
+}

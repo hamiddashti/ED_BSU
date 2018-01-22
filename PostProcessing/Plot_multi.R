6906578e497ab7a40da09b67891ca33ce80267e3
hamid4965 <ahangar.hamid@gmail.com>
2018-01-22 16:36:15 -0700

    s

--- PostProcessing/Plot_multi.R
+++ PostProcessing/Plot_multi.R
@@ -1,29 +1,42 @@
-setwd('/home/hamiddashti/bcal/Data02/bcal/Personal/hamid/ed/test/output_phen0/')
-df0<-ed_out(y1=2014,m1=01,d1=01,y2=2019,m2=12,d2=29,pfx = "hhh-D")
-save(df0,file='df0.rda')
-
-setwd('/home/hamiddashti/bcal/Data02/bcal/Personal/hamid/ed/test/output_phen-1/')
-df1<-ed_out(y1=2014,m1=01,d1=01,y2=2019,m2=12,d2=29,pfx = "hhh-D")
-save(df1,file='df1.rda')
-
-setwd('/home/hamiddashti/bcal/Data02/bcal/Personal/hamid/ed/test/output_phen2/')
-df2<-ed_out(y1=2014,m1=01,d1=01,y2=2019,m2=12,d2=29,pfx = "hhh-D")
-save(df2,file='df2.rda')
-
-setwd('/home/hamiddashti/bcal/Data02/bcal/Personal/hamid/ed/test/output_phen3/')
-df3<-ed_out(y1=2014,m1=01,d1=01,y2=2019,m2=12,d2=29,pfx = "hhh-D")
-save(df3,file='df3.rda')
-setwd('/home/hamiddashti/bcal/Data02/bcal/Personal/hamid/ed/test/output_phen-0-xml/')
-df4_xml<-ed_out(y1=2014,m1=01,d1=01,y2=2019,m2=12,d2=29,pfx = "hhh-D")
-save(df4_xml,file='df4.rda')
 
 
+setwd('/home/hamiddashti/bcal/Data02/bcal/Personal/hamid/ed/xml_test/xml_test4/')
+df5<-ed_out(y1=2010,m1=01,d1=01,y2=2014,m2=12,d2=29,pfx = "hhh-D")
+save(df5,file='df5.rda')
 
-t0 = df0$lai_co
-t1 = df1$lai_co
-t4 = df4_xml$lai_co
-plot (tmp,t0,ty='l')
-lines(tmp,t4,col='red')
+setwd('/home/hamiddashti/bcal/Data02/bcal/Personal/hamid/ed/test/holinv6/')
+df6<-ed_out(y1=2010,m1=01,d1=01,y2=2014,m2=12,d2=30,pfx = "hhh-D")
+save(df6,file='df6.rda')
+
+setwd('/home/hamiddashti/bcal/Data02/bcal/Personal/hamid/ed/test/holinv7/')
+df7<-ed_out(y1=2010,m1=01,d1=01,y2=2014,m2=12,d2=30,pfx = "hhh-D")
+save(df7,file='df7.rda')
+
+setwd('/home/hamiddashti/bcal/Data02/bcal/Personal/hamid/ed/test/holinv8/')
+df8<-ed_out(y1=2010,m1=01,d1=01,y2=2014,m2=12,d2=30,pfx = "hhh-D")
+save(df8,file='df8.rda')
+
+setwd('/home/hamiddashti/bcal/Data02/bcal/Personal/hamid/ed/test/holinv9/')
+df9<-ed_out(y1=2010,m1=01,d1=01,y2=2014,m2=12,d2=30,pfx = "hhh-D")
+save(df9,file='df9.rda')
+
+setwd('/home/hamiddashti/bcal/Data02/bcal/Personal/hamid/ed/test/holinv10/')
+df10<-ed_out(y1=2010,m1=01,d1=01,y2=2014,m2=12,d2=30,pfx = "hhh-D")
+save(df10,file='df10.rda')
+
+t5 = df5$dmean_gpp_co
+tmp=1:length(t5)
+t7 = df7$lai_co
+t8 = df8$lai_co
+t9 = df9$lai_co
+t10 = df10$lai_co
+
+plot (tmp,t5,ty='l')
+lines(tmp,t7,col='red')
+lines(tmp,t8,col='green')
+lines(tmp,t9,col='brown')
+lines(tmp,t10,col='blue')
+
 
 t2 = df2$lai_co
 t3 = df3$lai_co
